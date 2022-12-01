81adf72b831ba13ace7bd6996a65f5460d24c363
Shadya Gamal <shadya.gamal@epfl.ch>
2022-12-01 15:46:34 +0100

    Modèle hydro ok

---
+++ NS_fun.m
@@ -0,0 +1,5 @@
+function NS = NS_fun(Q_obs,Q_mod)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+NS = 1- ((sum((Q_obs-Q_mod).^2))/(sum((Q_obs-mean(Q_obs)).^2)));
+end
--- hydrological_model.m
+++ hydrological_model.m
@@ -1,18 +1,18 @@
-function [Q_mod,R,I] = hydrological_model(P, Q_obs, ET_0, kc, K_sat, c, t_sub, t_sup, z, sw, s1, n, Q_b, A)
+function [Q_mod,R,I] = hydrological_model(P, ET_0, kc, K_sat, c, t_sub, t_sup, z, sw, s1, n, Q_b, A,doTest)
 
 % This function computes the hydrological model (ie the discharge, runoff, infiltration,
 %saturation, leaching and potential evapotranspiration in [m^3/s]). It can run the hydrological
 %model for every hour (mode =0), every day in a month (mode =1), every month in a year (mode=2),
 % and all years (mode = 3), depending on the mode given.
-
-
-    
-    Nyears=length(Q_obs)/(365*24);                   % number of years
+% ALL UNITS MUST BE IN m and h
+  
+    Nyears=length(P)/(365*24);                       % number of years
+    N_hours_per_year = 365*24;                       % number of hours per year 
     day_month=[31 28 31 30 31 30 31 31 30 31 30 31]; % number of days for each month
     hour_month=day_month*24;                         % number of hours for each month
     
-    month_end=cumsum(day_month);                     % last day of each month
-    month_start=month_end-day_month+1;               % first day of each month
+    %month_end=cumsum(day_month);                     % last day of each month
+    %month_start=month_end-day_month+1;               % first day of each month
     
     month_end_hour=cumsum(hour_month);               % last hour of each month   
     month_start_hour=month_end_hour-hour_month+1;    % fisrt hour of each month
@@ -20,52 +20,74 @@ function [Q_mod,R,I] = hydrological_model(P, Q_obs, ET_0, kc, K_sat, c, t_sub, t
 
    
     % Settings
-    s = zeros(length(Q_obs),1);    % saturation vector
-    s(1) = 0.5;                    % initial arbitrary value
+    s = zeros(size(P));    % saturation vector
+    s(1) = 0.5;            % initial arbitrary value
     
-    Vsup = zeros(length(Q_obs),1);  % superficial stored volume vector 
-    
-    Vsub = zeros(length(Q_obs),1);  % sub-superficial stored volume vector
+    Q_mod = zeros(size(P));    %Discharge
+    R = zeros(size(P));    %Run off
+    I = zeros(size(P));    %Infiltration
+    L = zeros(size(P));    %Leaching
+    ET = zeros(size(P));   %Evapotranspiration
+    qsup = zeros(size(P)); %Superficial specific discharge
+    qsub = zeros(size(P)); %Sub-superficial specific discharge
+    Vsup = zeros(size(P)); %Superficial storage
+    Vsub = zeros(size(P)); %Sub-superficial storage
+    Qsup = zeros(size(P)); %Superficial discharge
+    Qsub = zeros(size(P)); %Sub-superficial discharge
+    dt = 1;
+   
 
-    N_hours_per_year = 365*24;      % number of hours per year 
-    ET_hourly = zeros(length(Q_obs),1); % evapotranspiration hourly [m/h]
-    L = zeros(length(Q_obs),1);     % leaching hourly [m/h]
-    
-    % Infiltration [m/h]
-    K_sat_vect = ones(length(P),1)*K_sat;  % K_sat vector to compare with precipitation
-    I = min(P,K_sat_vect); % [m/h]
-    
-    % Runoff
-    R = P-I; % [m/h]
     
     for y=1:Nyears
         for m=1:12
             for h=month_start_hour(m):month_end_hour(m)
-                t=(y-1)*N_hours_per_year+h; 
+                t=(y-1)*N_hours_per_year+h;
+
+                I(t)=min(P(t),K_sat); %m/h Infiltration
+                R(t)=P(t)-I(t); %m/h Runoff
+
                 if s(t)<=sw && s(t)>=0
-                    ET(t)= 0;
+                    ET(t)= 0; %m/h
                 elseif s(t)<=s1 && s(t)>sw
-                    ET(t)= kc(m)*ET_0(m)*((s(t)-sw)/(s1-sw));
+                    ET(t)= kc(m)*ET_0(m)*((s(t)-sw)/(s1-sw)); %m/h
                 elseif s(t)<=1 && s(t)>s1
-                    ET(t)= kc(m)*ET_0(m);
+                    ET(t)= kc(m)*ET_0(m); %m/h
                 elseif s(t)>1
                         disp('Warning : s > 1')
                 elseif s(t)<0
                         disp('Warning : s < 0')
                 end
-                L(t)=K_sat*(s(t)^c);
-                s(t+1)=s(t)+((I(t)-ET(t)-L(t))*(1/(n*z)));
-                Vsup(t+1)=Vsup(t)+R(t)-((t_sup^(-1))*Vsup(t));
-                Vsub(t+1)=Vsub(t)+L(t)-((t_sub^(-1))*Vsub(t));
+                L(t)=K_sat*(s(t)^c); %m/h
+                s(t+dt)=s(t)+(I(t)-ET(t)-L(t))*dt/(n*z);
+
+                qsup(t)=Vsup(t)/t_sup;
+                qsub(t)=Vsub(t)/t_sub;
+
+                Vsup(t+dt)=Vsup(t)+(R(t)-qsup(t))*dt; %m3
+                Vsub(t+dt)=Vsub(t)+(L(t)-qsub(t))*dt; %m3
+
+                Qsup(t)=A*qsup(t); % m3/h
+                Qsub(t)=A*qsub(t); % m3/h
+                Q_mod(t) = Qsub(t)+Qsup(t)+Q_b; % m3/h
             end
         end
     end
     
-    
-    qsup=Vsup/t_sup;
-    qsub=Vsub/t_sub;
-    Qsup=A*qsup/3.6;
-    Qsub=A*qsub/3.6;
-    Q_mod = Qsub+Qsup+Q_b;
+ %compute total discharge Q    
+if doTest     % if doTest=1, check mass balance 
 
+    P_tot=sum(P)*dt;
+    R_tot=sum(R)*dt;
+    L_tot=sum(L)*dt;
+    ET_tot=sum(ET)*dt;
+
+    %"testS" balance for the root zone (input/output). testS close to unity: necessary (but not sufficient) 
+    %condition for the implementation to be correct 
+    testS=P_tot/(ET_tot+R_tot+L_tot+n*z*(s(end)-s(1)))
+
+    %"testQ" balance for the whole system (input/output). testQ close to unity: necessary (but not sufficient) 
+    %condition for the implementation to be correct 
+    testQ=sum(P-ET)/(sum(qsup+qsub)+n*z*(s(end)-s(1))+Vsup(end)+Vsub(end))
+end   
+    
 end
